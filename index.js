b20fb1e142d95d7d8b8a3c1dbfff2e5bd6892857
Robin Kim <rkim1214@gmail.com>
2018-09-18 17:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-
 function add(a, b) {
-  a+b
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
 }
